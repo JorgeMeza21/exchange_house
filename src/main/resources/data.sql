dc206385e9431749e03acf2653758f755db68cdf
Jorge Meza <jorge.meza@latamready.com>
2025-01-21 19:19:31 -0500

    Se añadieron inserts sql para tabla de exchange types

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
 INSERT INTO roles (name) VALUES ('ROLE_USER');
 INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO exchange_types (source_currency, final_currency, conversion) VALUES ('PEN', 'USD', 0.287);
+INSERT INTO exchange_types (source_currency, final_currency, conversion) VALUES ('PEN', 'EUR', 0.257);
+INSERT INTO exchange_types (source_currency, final_currency, conversion) VALUES ('PEN', 'BRL', 1.663);
+INSERT INTO exchange_types (source_currency, final_currency, conversion) VALUES ('USD', 'PEN', 3.729);
+INSERT INTO exchange_types (source_currency, final_currency, conversion) VALUES ('USD', 'EUR', 0.958);
